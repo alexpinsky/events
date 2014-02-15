b02a707f67d93e4fc3dbac89e327027036f45f68
Alex Pinsky <alex_pinsky85@yahoo.com>
2014-02-15 14:00:59 +0200

    add cach dir for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,6 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAIQI3X75JJC22IZVQ',

f13c359a65ea10bc276fe4206a587883206eae79
Alex Pinsky <alex_pinsky85@yahoo.com>
2014-02-16 11:55:38 +0200

    fix for figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.cache_dir = 'carrierwave'
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Figaro.env.aws_access_key_id,
-    :aws_secret_access_key  => Figaro.env.aws_secret_access_key
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = 'events-assets-users'
 end

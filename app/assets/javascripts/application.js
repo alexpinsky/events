9e29bee42aebfec6e8ad2bb0e7a36739f5df7763
Alex Pinsky <alex_pinsky85@yahoo.com>
2014-07-14 12:52:25 +0300

    1. remove jplayer. 2. fix the bottom focus on welcome page. 3. add html audion tag.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.jplayer.min
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
 //= require jquery.minicolors.min
--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -1,7 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation_slim
-//= require events
-//= require jquery.jplayer.min
 
 $(function(){ $(document).foundation(); });

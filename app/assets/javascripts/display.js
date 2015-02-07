5d492e7149e8cff1a30a175ebbfe85e049181b15
Alex Pinsky <alex_pinsky85@yahoo.com>
2015-02-07 12:23:47 +0200

    1. verticaly center calssic theme on small portrait. 2. addjust max img heigh/width on large screen (classic).

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -2,8 +2,29 @@
 //= require jquery_ujs
 //= require foundation_slim
 $(document).foundation();
+var resizeFun = function() {
+  var docHeight = $('body').height();
+  console.log(docHeight);
+  if (docHeight <= 677 && docHeight >= 480) {
+    var imgHeight = $('img.main').height();
+    console.log(imgHeight);
+    var marginTop = (docHeight - imgHeight) / 2;
+    console.log(marginTop);
+    $('.theme-wrapper').css('margin-top', marginTop + 'px');
+  } else {
+    $('.theme-wrapper').css('margin-top', 0);
+  }
+};
+
 $(document).ready(function() {
   $('span').click(function() {
     ga('send', 'event', { eventCategory: 'invites', eventAction: 'add to calendar'});
   });
+
+  $(window).resize(resizeFun);
 });
+
+$(window).load(function() {
+  resizeFun();
+});
+

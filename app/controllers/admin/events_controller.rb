220491b6a9a79b5c98e2cd8a8b1ca5508e64622d
Alex Pinsky <alex_pinsky85@yahoo.com>
2015-02-23 02:14:09 +0200

    fix theme stats view and order by created_at for admin/events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,6 +1,6 @@
 class Admin::EventsController < ApplicationController
 
   def index
-    @events = Event.includes(:theme).with_url
+    @events = Event.includes(:theme).with_url.order('created_at desc')
   end
 end

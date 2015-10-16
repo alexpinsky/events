76705db1209d222787c579a0ed6d5a49271f7927
Alex Pinsky <alex_pinsky85@yahoo.com>
2015-10-16 16:11:15 +0300

    add coming soon images for missing categories

--- app/assets/javascripts/templates.js.coffee
+++ app/assets/javascripts/templates.js.coffee
@@ -10,7 +10,7 @@ class @Templates
     @initTileListeners()
 
   initTileListeners: ->
-    @container.find('.tile.exist').each (i, obj) ->
+    @container.find('.tile').each (i, obj) ->
       $(obj).hover ((e) =>
         tile_obj = $(e.currentTarget)
         tile_obj.find('.menu-wrapper').css("visibility", "visible")
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,17 +3,5 @@ class CategoriesController < ApplicationController
   def index
     # {'wedding' => [event, event..], 'meetups' => [event, event,...], ...}
     @templates = Event.themes.active.include_categories.group_by(&:category_name)
-    _prepend_placeholder_templates @templates
-  end
-
-  private
-
-  def _prepend_placeholder_templates(templates)
-    mock_template = Event.new
-    templates.merge!(
-      'celebration' => [mock_template, mock_template, mock_template, mock_template],
-      'meetups'     => [mock_template, mock_template, mock_template, mock_template],
-      'whatever'    => [mock_template, mock_template, mock_template, mock_template]
-    )
   end
 end

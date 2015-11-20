a2e493470ac5582153951071748db46186de6880
Alex Pinsky <alex_pinsky85@yahoo.com>
2015-11-21 00:21:25 +0200

    Add admin view for contact requests

--- app/controllers/admin/contact_requests_controller.rb
+++ app/controllers/admin/contact_requests_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ContactRequestsController < ApplicationController
 
   def index
+    @requests = ContactRequest.order('created_at desc')
   end
 
   def show
+    @request = ContactRequest.find params[:id]
   end
 end

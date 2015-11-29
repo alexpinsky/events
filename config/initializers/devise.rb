54bcd0d780f3cbfa3222bdcdf799e950fc1f7b1b
Alex Pinsky <alex_pinsky85@yahoo.com>
2015-11-29 22:45:59 +0200

    add photos to FB user permissions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-                  scope: 'email', info_fields: 'email, name, photos',
+                  scope: 'email, user_photos',
                   callback_url: ENV['FACEBOOK_CALLBACK_URL']
 
   # ==> Warden configuration

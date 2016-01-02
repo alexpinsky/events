bea559728c9b29c4f0e3f1b8a6826b85267523d9
Alex Pinsky <alex_pinsky85@yahoo.com>
2016-01-02 15:59:14 +0200

    remove react init from dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,4 @@ Events::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.react.jsx_transformer_class = React::JSX::JSXTransformer
-  config.react.variant = :development
 end

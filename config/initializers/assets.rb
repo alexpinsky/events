b30c621634be04fafd1a602717774b50c9c79ed0
Alex Pinsky <alex_pinsky85@yahoo.com>
2016-01-03 20:42:30 +0200

    wip

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( display.css )
 Rails.application.config.assets.precompile += %w( display.js )
-Rails.application.config.assets.precompile += %w( play.js )
+# Rails.application.config.assets.precompile += %w( play.js )

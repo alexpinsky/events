514965a0d496687ff6a92af6709478111525d27f
Alex Pinsky <alex_pinsky85@yahoo.com>
2016-02-12 11:06:41 +0200

    play around

--- app/assets/javascripts/play/index.js
+++ app/assets/javascripts/play/index.js
@@ -4,10 +4,38 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/app';
+import Hello from './components/hello';
 import reducers from './reducers';
 
+// ReactDOM.render(
+//   <Provider store={createStore(reducers)}>
+//     <App />
+//   </Provider>,
+//   document.querySelector('#play')
+// );
+
+class ComponentMapper {
+
+  static map(klass) {
+
+    switch(klass) {
+      case 'Hello':
+        return Hello;
+      default:
+        return null;
+    }
+  }
+}
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
-  document.querySelector('#play'));
+  React.createElement(ComponentMapper.map('Hello'),{
+    text: 'Alex Pinsky'
+  }),
+  document.querySelector('#play')
+);
+
+// const x = {};
+// console.log('x: ', x);
+// x.a = 1;
+// console.log('x: ', x);
+

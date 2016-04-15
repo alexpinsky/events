bf9e6b2efcde9993406b0d1850e87741945ce8fd
Alex Pinsky <alex_pinsky85@yahoo.com>
2016-04-15 12:19:44 +0300

    Add new log formatter to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Events::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.logger = Logger.new(STDOUT)
+  config.logger.formatter = Logging::Formatters::Clean.new
 end
